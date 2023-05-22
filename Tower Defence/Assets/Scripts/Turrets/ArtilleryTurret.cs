39d47962f588dc2f6b2bece6d00f23b20a9d1c22
Aminev Ural <aminevural51@gmail.com>
2023-05-22 22:16:04 +0300

    Пофиксил Sans

--- Tower Defence/Assets/Scripts/Turrets/ArtilleryTurret.cs
+++ Tower Defence/Assets/Scripts/Turrets/ArtilleryTurret.cs
@@ -2,5 +2,12 @@
 
 public class ArtilleryTurret : BaseTurret
 {
+    [field:SerializeField] public float ExplosionRange { get; protected set; }
     
+    protected override void Shoot()
+    {
+        var firedBullet = Instantiate(BulletPrefab, BulletShootPos.transform.position, transform.rotation);
+        var bulletScript = firedBullet.GetComponent<ArtilleryTurretProjectile>();
+        bulletScript.InitializeProps(ProjectileSpeed, ProjectileDamage, Target, ExplosionRange);
+    }
 }
