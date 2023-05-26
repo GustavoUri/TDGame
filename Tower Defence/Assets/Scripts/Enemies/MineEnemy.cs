2f73015736f11d2a9bc3d391ea404b224348750a
Aminev Ural <aminevural51@gmail.com>
2023-05-26 22:53:41 +0300

    Переделал UI

--- Tower Defence/Assets/Scripts/Enemies/MineEnemy.cs
+++ Tower Defence/Assets/Scripts/Enemies/MineEnemy.cs
@@ -5,9 +5,11 @@ public class MineEnemy : BaseEnemy
     
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("MainTower")) return;
-        TowerScript.GetDamage(Damage, gameObject.tag);
-        Destroy(gameObject);
+        if (other.CompareTag("MainTower"))
+        {
+            TowerScript.GetDamage(Damage, gameObject.tag);
+            Destroy(gameObject);
+        }
 
     }
 }
