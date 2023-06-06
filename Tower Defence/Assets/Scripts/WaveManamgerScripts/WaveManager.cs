46d62560b4edeaa3729e5bdf463bba9db2b3ef39
LeonardDE <55032590+LeonardDE@users.noreply.github.com>
2023-06-06 11:48:58 +0500

    Level_1 and changeWaveMananger
    Начал делать Первый уровень и также изменил систему волн

--- Tower Defence/Assets/Scripts/WaveManamgerScripts/WaveManager.cs
+++ Tower Defence/Assets/Scripts/WaveManamgerScripts/WaveManager.cs
@@ -4,36 +4,53 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
+
     [System.Serializable] public class SubList{
         public string WaveName;
-        public float waitTimeAfterWave;
-        public float timeBetween;
+        [Range(0,60)]public float waitTimeAfterWave;
+        [Range(0,60)]public float timeBetween;
         public Transform spawnPoint;
-        public List<GameObject> Wave = new List<GameObject>();
+        public List<wavePart> Wave = new List<wavePart>();
+    }
+
+    [System.Serializable] public class wavePart{
+        [Range(0,100)]public int count;
+        public GameObject enemy;
     }
     [SerializeField] private List<SubList> Waves;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(WavesSpawner(Waves));
+        
     }
 
     private IEnumerator WavesSpawner(List<SubList> waves){
         for(int i = 0; i <waves.Count;i++){
             yield return new WaitForSeconds(waves[i].waitTimeAfterWave);
-            StartCoroutine(WaveSpawner(waves[i].Wave,waves[i].timeBetween,waves[i].spawnPoint.position));
+            //StartCoroutine(WaveSpawner(waves[i].Wave,waves[i].timeBetween,waves[i].spawnPoint.position));
+            yield return StartCoroutine(WaveSpawner(waves[i].Wave,waves[i].timeBetween,waves[i].spawnPoint.position));
+            
+            yield return StartCoroutine(WaitEndWave());
+
+            
         }
        
     }
-    private IEnumerator WaveSpawner(List<GameObject> wave,float timeBetween,Vector3 spawnPoint){
+    private IEnumerator WaveSpawner(List<wavePart> wave,float timeBetween,Vector3 spawnPoint){
         for(int i = 0; i<wave.Count;i++){
             yield return new WaitForSeconds(timeBetween);
-            EnemySpawner(wave[i],spawnPoint);
+            for(int k =0;k<wave[i].count;k++){
+                EnemySpawner(wave[i].enemy,spawnPoint);
+            }
         }
     }
     private void EnemySpawner(GameObject enemy,Vector3 spawnPoint){
         Instantiate(enemy,spawnPoint,Quaternion.identity);
     }
-
-
+    private IEnumerator WaitEndWave(){
+        yield return new WaitUntil(() => GameObject.FindWithTag("Enemy") == null);
+    }
 }
+
+
